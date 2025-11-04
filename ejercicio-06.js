60092be3b757f9e9d7572727096692e06e673a95
NahlaGarcia <nahla.daw@outlook.es>
2025-11-04 19:21:35 +0100

    fin ejercicio 06

---
+++ ejercicio-06.js
@@ -0,0 +1,24 @@
+//Ejercicio 6
+//1.1 Crea un bucle for que vaya desde 0 a 9 y muestra el valor de i por consola.
+for (let index = 0; index < 10; index++) {
+    console.log(index);
+}
+
+/*1.2 Crea un bucle for que vaya desde 0 a 9 y muestra el valor de i por consola solo
+cuando el resto del numero dividido entre 2 sea 0.*/
+for (let index = 0; index < 10; index++) {
+    if (index % 2 == 0) {
+        console.log(index);
+    }
+}
+
+/*1.3 Crea un bucle para conseguir dormir contando ovejas.
+Este bucle tiene que dar 10 vueltas, es decir, 10 console.log.
+Muestra por consola un mensaje diciendo 'Intentando dormir 🐑' en cada vuelta del bucle y cambia el mensaje en la décima vuelta a '¡Dormido!'.*/
+for (let index = 0; index <= 10; index++) {
+    if (index == 10) {
+        console.log("¡Dormido!");        
+    }  else {
+        console.log("Intentando dormir 🐑");
+    }
+}
