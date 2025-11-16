bd360ed089e2cd3b047aee84725aca97c701dd3c
NahlaGarcia <nahla.daw@outlook.es>
2025-11-16 20:24:06 +0100

    fin hasta ejercicio 38

---
+++ ejercicios-basicos-avanzados/ejercicio-38.js
@@ -0,0 +1,52 @@
+//Ejercicio 38
+
+/*Dado el siguiente código usa **forof** y **forin** para hacer la media del volumen de todos los sonidos favoritos que tienen los usuarios.
+Es decir, la media de volumen de todos los volumenes juntos.*/
+
+const users = [
+  {
+    name: "Alberto",
+    favoritesSounds: {
+      waves: { format: "mp3", volume: 50 },
+      rain: { format: "ogg", volume: 60 },
+      firecamp: { format: "mp3", volume: 80 },
+    },
+  },
+  {
+    name: "Antonio",
+    favoritesSounds: {
+      waves: { format: "mp3", volume: 30 },
+      shower: { format: "ogg", volume: 55 },
+      train: { format: "mp3", volume: 60 },
+    },
+  },
+  {
+    name: "Santiago",
+    favoritesSounds: {
+      shower: { format: "mp3", volume: 50 },
+      train: { format: "ogg", volume: 60 },
+      firecamp: { format: "mp3", volume: 80 },
+    },
+  },
+  {
+    name: "Laura",
+    favoritesSounds: {
+      waves: { format: "mp3", volume: 67 },
+      wind: { format: "ogg", volume: 35 },
+      firecamp: { format: "mp3", volume: 60 },
+    },
+  },
+];
+
+let volumenes = 0;
+let contadorVolumenes = 0;
+
+for (const user of users) {
+    for (const sonido in user.favoritesSounds){
+        const volumen = user.favoritesSounds[sonido].volume;
+        volumenes += volumen;
+        contadorVolumenes ++;
+    }
+}
+let mediaVolumenes = volumenes / contadorVolumenes;
+console.log("La media del volumen de todos los sonidos favoritos que tienen los usuarios es: "+ mediaVolumenes);
