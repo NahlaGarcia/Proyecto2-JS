a07fc7aed148e9464c4951cbac3b2b52e0cf5304
NahlaGarcia <nahla.daw@outlook.es>
2026-02-11 19:07:53 +0100

    fin del ejercicio 02

--- ejercicios-dom/ejercicio-02/main.js
+++ ejercicios-dom/ejercicio-02/main.js
@@ -26,12 +26,41 @@ texto.textContent ='Soy dinámico';
 document.body.appendChild(texto);
 
 //2.5 Inserta en el h2 con la clase.fn-insert-here el texto'Wubba Lubba dub dub'.
+const h2 = document.querySelector('.fn-insert-here');
+h2.textContent = 'Wubba Lubba dub dub';
 
 //2.6 Basandote en el siguiente array crea una lista ul> li con los textos del array.
 const apps=['Facebook','Netflix','Instagram','Snapchat','Twitter'];
 
+const ul = document.createElement('ul');
+for (let i = 0; i < apps.length; i++) {
+    const li = document.createElement('li');
+    li.textContent = apps[i];
+    ul.appendChild(li);
+}
+
+document.body.appendChild(ul);
+
 //2.7 Elimina todos los nodos que tengan la clase.fn-remove-me
+const elementos = document.querySelectorAll('.fn-remove-me');
+for (const elemento of elementos) {
+    elemento.remove();
+    
+}
 
 //2.8 Inserta una p con el texto'Voy en medio!' entre los dos div. Recuerda que no solo puedes insertar elementos con.appendChild.
+const enMedio = document.createElement('p');
+enMedio.textContent = 'Voy en medio!';
+
+const divs = document.querySelectorAll('div');
+divs[0].insertAdjacentElement('afterend', enMedio);
 
 //2.9 Inserta p con el texto'Voy dentro!', dentro de todos los div con la clase .fn-insert-her
+
+const divsInsert = document.querySelectorAll('.fn-insert-here');
+for (const divInsert of divsInsert) {
+    const text = document.createElement ('p');
+    text.textContent = 'Voy dentro!';
+    divInsert.appendChild(text);
+    
+}
